885d6839b2952ebc7179918633d0bc4f023ccf96
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-11-22 22:08:53 +1300

    Created components and formed basic structure of website in App.jsx

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Intro from './Intro';
+import About from './About';
+import TechStack from './TechStack';
+import Projects from './Projects';
+import Contact from './Contact';
+
+export {
+  Intro,
+  About,
+  TechStack,
+  Projects,
+  Contact
+}
