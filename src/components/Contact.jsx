f12b41cd8921a665b5bb3c88a1b7f6479e558924
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-11-23 21:49:38 +1300

    feat: Contact form structure and styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import { useState, useRep, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+import { styles } from '../styles';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => { }
+
+  const handleSubmit = (e) => { }
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row gap-10 overflow-hidden w-full h-screen flex justify-center items-center">
+      <div className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
+        <form 
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-8 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="Your name"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Email</span>
+            <input
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Your email"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Message</span>
+            <textarea
+              rows="6"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="Message"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,13 +2,11 @@ import { styles } from '../styles';
 
 const Intro = () => {
   return (
-  <section className="relative w-full h-screen mx-auto">
-    <div className={`${styles.paddingX} w-full h-screen flex justify-center items-center`}>
+  <section className="w-full h-screen flex justify-center items-center">
       <div>
         <h1 className={`${styles.heroHeadText} text-white`}><span className="text-[#5e69ff]">Tony Lim</span></h1>
         <h2 className={`${styles.heroSubText} text-white-100`}>Software engineer based in Auckland, New Zealand</h2>
       </div>
-    </div>
   </section>
   )
 }
