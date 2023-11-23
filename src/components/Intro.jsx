f65270e4edab5fd0d335941fa5e07eba5a8bc8f5
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-11-23 13:16:33 +1300

    Responsive design and text for the intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { styles } from '../styles';
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+  <section className="relative w-full h-screen mx-auto">
+    <div className={`${styles.paddingX} w-full h-screen flex justify-center items-center`}>
+      <div>
+        <h1 className={`${styles.heroHeadText} text-white`}><span className="text-[#5e69ff]">Tony Lim</span></h1>
+        <h2 className={`${styles.heroSubText} text-white-100`}>Software engineer based in Auckland, New Zealand</h2>
+      </div>
+    </div>
+  </section>
   )
 }
 
--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,12 @@
 const styles = {
-    paddingX: "sm:px-16 px-6",
+    paddingX: "sm:px-16",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
   
     heroHeadText:
-      "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+      "font-black text-white lg:text-[150px] sm:text-[100px] xs:text-[80px] text-[60px] lg:leading-[95px] lg: mt-2 text-center",
     heroSubText:
-      "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+      "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] lg:mt-16 text-center",
   
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
