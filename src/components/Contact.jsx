04eebfa7f62acd1a52aa8fa6e1407ac570eff729
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-11-23 22:13:01 +1300

    feat: emailjs functionality, send and reset form as well as error handling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,16 +12,53 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => { }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  }
 
-  const handleSubmit = (e) => { }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // emailjs IDs
+    // D8OsdM0fe8rYe2tFv
+    // template_y49vb0t
+    // service_zw19n7e
+
+    emailjs.send(
+      'service_zw19n7e', 
+      'template_y49vb0t', 
+      {
+        from_name: form.name,
+        to_name: 'Tony',
+        from_email: form.email,
+        to_email: 'tonylim8669@gmail.com',
+        message: form.message
+      },
+      'D8OsdM0fe8rYe2tFv'
+      )
+      .then(() => {
+        setLoading(false);
+        alert('Your message has been sent!');
+        setForm({
+          name: '',
+          email: '',
+          message: ''
+        })
+      }, (error) => {
+        setLoading(false);
+        console.log(error);
+        alert('Something went wrong, please try again.');
+      })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row gap-10 overflow-hidden w-full h-screen flex justify-center items-center">
       <div className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
-        <form 
+        <form
           ref={formRef}
           onSubmit={handleSubmit}
           className="mt-8 flex flex-col gap-8"
