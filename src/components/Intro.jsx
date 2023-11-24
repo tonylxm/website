6f76e2b15531bfa223cc431afeada93d9722df47
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-11-25 10:08:38 +1300

    refactor: Changed colour of name

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
   <section className="w-full h-screen flex justify-center items-center">
       <div>
-        <h1 className={`${styles.heroHeadText} text-white`}><span className="text-[#5e69ff]">Tony Lim</span></h1>
+        <h1 className={`${styles.heroHeadText} text-white`}><span className="text-[#5f69ff]">Tony Lim</span></h1>
         <h2 className={`${styles.heroSubText} text-white-100`}>Software engineer based in Auckland, New Zealand</h2>
       </div>
   </section>
--- src/styles.js
+++ src/styles.js
@@ -6,7 +6,7 @@ const styles = {
     heroHeadText:
       "font-black text-white lg:text-[120px] sm:text-[100px] xs:text-[80px] text-[60px] lg:leading-[95px] lg: mt-2 text-center",
     heroSubText:
-      "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] lg:mt-16 text-center",
+      "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] lg:mt-12 text-center",
   
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
