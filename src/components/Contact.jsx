d74e279dd1e8f099726ad541182c3915733b8345
tonylxm <126369686+tonylxm@users.noreply.github.com>
2023-12-04 22:20:55 +1300

    refactor: updated colour changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -58,7 +58,7 @@ const Contact = () => {
     <motion.div 
     variants={{
         hidden: { opacity: 0, y: 100 },
-        show: { opacity: 1, y: 0 }
+        show: { opacity: 1, y: 0 },
     }}
       initial="hidden"
       whileInView="show"
