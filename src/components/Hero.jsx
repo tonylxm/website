7c54411577c431697d490609f27339d25b37c2a1
tonylxm <tony@emergencyq.co.nz>
2023-12-08 00:48:20 +1300

    feat: parallax scrolling in about section. Name and title go behind rangitoto image on scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,57 @@
 import { styles } from '../styles';
 import { useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
+import rangitotoTop from '../assets/rangitoto-top.png';
+import rangitotoBottom from '../assets/rangitoto-bottom.jpg';
 
 const Hero = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
+  const ref1 = useRef(null);
+  const isInView = useInView(ref1);
+
+  const ref2 = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref2,
+    offset: ["start start", "end start"],
+  });
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "1000%"]);
 
   return (
-    <section className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen overflow-hidden relative grid place-items-center">
       <motion.div
+        style={{ y: textY }}
         variants={{
           hidden: { opacity: 0 },
           fadeIn: { opacity: 1 },
         }}
-        ref={ref}
+        ref={ref1}
         initial="hidden"
-        animate={ isInView ? "fadeIn" : "hidden"}
+        animate={isInView ? "fadeIn" : "hidden"}
         transition={{ duration: 0.5, delay: 0.15 }}
+        className="relative z-10"
       >
-        <h1 className={`${styles.heroHeadText} text-white`}><span className="text-[#5f69ff]">Tony Lim</span></h1>
-        <h2 className={`${styles.heroSubText} text-white-100`}>Software engineer based in Auckland, New Zealand</h2>
+        <h1 className={`${styles.heroHeadText}`}><span className="text-[#ffffff]">Tony Lim</span></h1>
+        <h2 className={`${styles.heroSubText} text-[#ffffff]`}>Software engineer based in Auckland, New Zealand</h2>
       </motion.div>
-    </section>
+
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `url(${rangitotoBottom})`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          y: backgroundY,
+        }}
+      />
+      <div
+        className="absolute inset-0 z-20"
+        style={{
+          backgroundImage: `url(${rangitotoTop})`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+      />
+    </div>
   )
 }
 
