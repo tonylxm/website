0c2b25ee514fce90e8ee5b3a55f8907ae5095920
tonylxm <tony@emergencyq.co.nz>
2023-12-14 02:31:48 +1300

    styles adjustment for text & sbg size

--- src/styles.js
+++ src/styles.js
@@ -1,17 +1,17 @@
 const styles = {
-  screen: "w-full h-[720px] h-screen flex justify-center items-center overflow-hidden", //
+  screen: "w-full h-screen flex justify-center items-center overflow-hidden", // h-[720px]
   card: "bg-secondary p-4 rounded-2xl shadow-card",
-  button: "btn bg-primary py-3 px-8 text-[12px] sm:text-[14px] md:text-[16px] lg:text-lg outline-none border-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl hover:bg-indigo-950",
-  svg: "h-4 sm:md:h-5 lg:xl:h-6",
+  button: "btn bg-primary py-3 px-8 text-[15px] sm:text-[16px] md:text-[17px] lg:text-lg outline-none border-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl hover:bg-indigo-950",
+  svg: "h-5 md:h-6",
 
   heroHeadText: "font-black text-white text-[70px] sm:text-[80px] md:text-[90px] lg:text-[120px] lg:leading-[95px] text-center",
   heroSubText: "text-[#d2dcff] mx-20 font-medium text-[16px] sm:text-[18px] md:text-[20px] lg:text-[30px] lg:leading-[40px] lg:mt-12 text-center",
 
   sectionHeadText: "text-white font-black text-[30px] sm:text-[50px] md:text-[60px]",
-  sectionText: "text-[12px] sm:text-[14px] md:text-[16px] lg:text-lg",
+  sectionText: "text-[15px] sm:text-[16px] md:text-[17px] lg:text-lg",
 
-  formSubText: "text-white font-medium mb-2 text-[12px] sm:text-[14px] md:text-[16px] lg:text-lg",
-  formText: "bg-primary py-3 px-3 text-[12px] sm:text-[14px] md:text-[16px] lg:text-lg placeholder:text-tertiary text-white rounded-lg outline-none border-none font-medium focus:bg-indigo-950",
+  formSubText: "text-white font-medium mb-2 text-[15px] sm:text-[16px] md:text-[17px] lg:text-lg",
+  formText: "bg-primary py-3 px-3 text-[15px] sm:text-[16px] md:text-[17px] lg:text-lg placeholder:text-tertiary text-white rounded-lg outline-none border-none font-medium focus:bg-indigo-950",
 };
 
 export { styles };
